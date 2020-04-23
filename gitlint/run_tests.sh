f2721fcef994601ee3aa949596dc0e48edd69fd1
Andrii Holovchenko <andriih@mellanox.com>
2020-04-23 10:39:51 +0000

    show vars
    Signed-off-by: Andrii Holovchenko <andriih@mellanox.com>

--- gitlint/run_tests.sh
+++ gitlint/run_tests.sh
@@ -24,6 +24,18 @@ BLUE="\033[94m"
 GREEN="\033[32m"
 NO_COLOR="\033[0m"
 
+echo "ghprbActualCommit $ghprbActualCommit"
+echo "ghprbActualCommitAuthor $ghprbActualCommitAuthor"
+echo "ghprbActualCommitAuthorEmail $ghprbActualCommitAuthorEmail"
+echo "ghprbPullDescription $ghprbPullDescription"
+echo "ghprbPullId $ghprbPullId"
+echo "ghprbPullLink $ghprbPullLink"
+echo "ghprbPullTitle $ghprbPullTitle"
+echo "ghprbSourceBranch $ghprbSourceBranch"
+echo "ghprbTargetBranch $ghprbTargetBranch"
+echo "ghprbCommentBody $ghprbCommentBody"
+echo "sha1 $sha1"
+
 run_git_check(){
     echo -ne "Running gitlint...${RED}"
     RESULT=$(gitlint 2>&1)
